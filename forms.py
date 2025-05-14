e9875cbd830f9bdf19315bda95a52fbfaad51d4b
dioxsound <dioxsound@gmail.com>
2025-05-14 21:04:05 +0300

    all done

--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ from flask_wtf.file import FileAllowed
 class ScenarioForm(FlaskForm):
     text = TextAreaField('Текст сценария', validators=[DataRequired()], render_kw={"id": "text"})
     image = FileField('Изображение', validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Только изображения!')])
-    is_start = BooleanField('Это начало новой игры?')  # НОВОЕ ПОЛЕ
+    is_start = BooleanField('Это начало новой игры?')
     submit = SubmitField('Сохранить')
 
 class ChoiceForm(FlaskForm):
